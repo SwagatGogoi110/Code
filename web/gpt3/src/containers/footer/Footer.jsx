bcc8d09f28e1d32083b251daed223a1dfc4461c5
SwagatGogoi110 <85822163+SwagatGogoi110@users.noreply.github.com>
2022-08-20 13:49:18 +0530

    Update Footer.jsx

--- web/gpt3/src/containers/footer/Footer.jsx
+++ web/gpt3/src/containers/footer/Footer.jsx
@@ -36,9 +36,9 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>Swagat Gogoi</p>
+        <p>9678739897</p>
+        <p>sg110.official@gmail.com</p>
       </div>
     </div>
 
